10077850bd39a3702948158fa2c59508a226e4a9
chiennguyen30 <nguyenchien30102001@gmail.com>
2024-03-07 17:40:59 +0700

    paginate user

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,44 +1,72 @@
 import React, { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
 
 import Table from "react-bootstrap/Table";
 import { fetchAllUser } from "../services/UserService";
 
 export const TableUsers = () => {
   const [listUser, setListUser] = useState([]);
-
+  const [totalUsers, setTotalUsers] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
   useEffect(() => {
-    getUsers();
+    getUsers(1);
   }, []);
-  const getUsers = async () => {
-    let res = await fetchAllUser();
+  const getUsers = async (page) => {
+    let res = await fetchAllUser(page);
     if (res && res.data) {
+      console.log(res);
+      setTotalUsers(res.total);
+      setTotalPages(res.total_pages);
       setListUser(res.data);
     }
   };
+  const handlePageClick = (even) => {
+    getUsers(+even.selected + 1);
+  };
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Email</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-        </tr>
-      </thead>
-      <tbody>
-        {listUser &&
-          listUser.length > 0 &&
-          listUser.map((item, index) => {
-            return (
-              <tr key={`users-${index}`}>
-                <td>{item.id}</td>
-                <td>{item.email}</td>
-                <td>{item.first_name}</td>
-                <td>{item.last_name}</td>
-              </tr>
-            );
-          })}
-      </tbody>
-    </Table>
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Email</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listUser &&
+            listUser.length > 0 &&
+            listUser.map((item, index) => {
+              return (
+                <tr key={`users-${index}`}>
+                  <td>{item.id}</td>
+                  <td>{item.email}</td>
+                  <td>{item.first_name}</td>
+                  <td>{item.last_name}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </Table>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={totalPages}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+      />
+    </>
   );
 };
